f88a1bcca1dbbe9d7f1647d8d68675871fd6590a
Sibusiso Gumede <sibusisojgumede@gmail.com>
2024-02-27 03:45:32 +0200

    Made project configurations: database setup.

--- dmg_django_app/urls.py
+++ dmg_django_app/urls.py
@@ -1,23 +1,13 @@
-"""
-URL configuration for dmg_django_app project.
+"""Defines URL patterns for dmg_django_app."""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    #path("admin/", include('admin.site.urls')),
-    path("", include(('dmg_django_app1.urls', 'dmg_django_app1'), namespace='dmg_django_app1')),
+    # Home page
+    path(r'', views.groceryItemsForm, name='home'),
+    # Category products page.
+    path(r'category_products/', views.bestCategoryProducts, name='categories'),
+    # Best discounted products.
+    path(r'best_discounted_products/', views.bestDiscountedProducts, name='bestdiscounted'),
 ]
--- dmg_django_app1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DmgDjangoApp1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dmg_django_app1'
