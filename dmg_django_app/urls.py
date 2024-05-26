1c851e6948e0ac3819f77454a0f1f568a119e52f
Sibusiso Gumede <sibusisojgumede@gmail.com>
2024-05-26 13:11:25 +0200

    DEVELOPMENTS: added a jumbotron to the layout in the near_me template.

--- dmg_django_app/urls.py
+++ dmg_django_app/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # Home page
     path('', views.homepage, name='home'),
     # Receipt renderer page.
-    path('home/receiptify', views.receiptify, name='receiptify'),
+    path('receiptify', views.receiptify, name='receiptify'),
     # Discounted products page.
     path('home/discounted_products/', views.discounted_products, name='discounted_products'),
     # Nearby supermarkets page.
-    path('home/near_me', views.near_me, name='near_me')
+    path('near_me', views.near_me, name='near_me')
 ]
