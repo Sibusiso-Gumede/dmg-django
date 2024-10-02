011497b9529de8ad66af9b0d5aa846b0c7d43137
Sibusiso Gumede <sibusisojgumede@gmail.com>
2024-10-02 11:43:34 +0200

    Deployment configurations.

--- dmg_django/settings.py
+++ dmg_django/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL =  '/static/'
-STATIC_ROOT = f'{BASE_DIR.as_posix()}/static'
+STATIC_ROOT = f'{BASE_DIR.as_posix()}/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
